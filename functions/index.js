5199cb8d401f1ef0f963a5f4d77f92703d4bb96f
Teemu Mäntyharju <teemu.mantyharju@houston-inc.com>
2018-01-22 08:17:08 +0200

    return user's public ip address

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => 
+{
+    const data = {
+        ip: req.ip,
+        forwardedFor: req.headers["x-forwarded-for"].split(",")[0],
+        connectionRemoteAddress: req.connection.remoteAddress,
+        socketRemoteAddress: req.socket.remoteAddress
+    }
+
+    res.send("Your public IP address is: " + data.forwardedFor);
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+exports.app = functions.https.onRequest(app);
